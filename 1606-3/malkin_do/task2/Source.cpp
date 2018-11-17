e891c3ff38a091e9848cb608512437f768f4f894
SeniorMalkin <senior.malkin2014@yandex.ru>
2018-11-17 16:24:15 +0300

    generalization for any root

--- 1606-3/malkin_do/task2/Source.cpp
+++ 1606-3/malkin_do/task2/Source.cpp
@@ -34,43 +34,40 @@ void My_Bcast(void* buffer, int count, MPI_Datatype datatype, int root, MPI_Comm
 	MPI_Comm_rank(comm, &currRank);
 
 	int ther = powTwo(psize);
-	
+
+	//process wait
 	if (currRank != root) {
 		MPI_Recv(buffer, count, datatype, MPI_ANY_SOURCE,MPI_ANY_TAG, comm, &status);
 	}
-	else {
-		if (root != 0) {
-			MPI_Send(buffer, count, datatype, 0, 0, comm);
-		}
-	}
 
-	/*
-	for (int i = 0; i < ther - 1; i++) {
-		tut = 1 << i;
-		for (int j = 0; j < tut; j++) {
-				if ((currRank == j) && ((j+tut)!= root))
-					MPI_Send(buffer, count, datatype, j + tut, 0, comm);
-		}
-
-	}
-	*/
 	
-		
+	if (root != 0) {
+
+		if (currRank < root) {
+			currRank += (psize - root);
+		}
+		else {
+			currRank -= root;
+		}
+
+	}
+
 	for (int i = powTwo(currRank); (currRank | (1 << i)) < psize; i++) {
-		if ((currRank | (1 << i)) != root)
-		MPI_Send(buffer, count, datatype, currRank | (1 << i), 0, comm);
-	}
-	
-	
-	/*
-	tut = 1 << (ther - 1);
-	for (int i = 0; i < psize - tut; i++) {
-		if (i + tut != root) {
-			if (currRank == i)
-				MPI_Send(buffer, count, datatype, i + tut , 0, comm);
+
+		int proc = (currRank | (1 << i));
+		if (root != 0) {
+			if (proc >= (psize - root)) {
+				proc -= (psize - root);
+			}
+			else {
+				proc += root;
+			}
 		}
+
+		MPI_Send(buffer, count, datatype, proc, 0, comm);
+			
 	}
-	*/
+	
 }
 
 
@@ -78,7 +75,7 @@ void main(int argc, char* argv[])
 {
 	int ProcRank;
 	int psize = 0;
-	double* test = nullptr;
+	int* test = nullptr;
 	double startwtime = 0.0;
 	double endwtime , startwtime1,endwtime1;
 	int tut = powTwo(5);
@@ -86,28 +83,26 @@ void main(int argc, char* argv[])
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &psize);
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-	test = new double[4];
-	if (ProcRank == 2){
+	test = new int[5];
+	if (ProcRank == 3){
 		test[0] = 1.12;
 		test[1] = 1.23;
 		test[2] = 2.44;
 		test[3] = 3.12;
+		test[4] = 7.17;
 	}
-	if(ProcRank == 0)
+
 	startwtime = MPI_Wtime();
-	My_Bcast(test,4, MPI_DOUBLE,2, MPI_COMM_WORLD);
-	if (ProcRank == 0)
+	My_Bcast(test,5, MPI_INT,3, MPI_COMM_WORLD);
 	endwtime = MPI_Wtime();
 
-	if (ProcRank == 0)
 	startwtime1 = MPI_Wtime();
-	MPI_Bcast(test, 4, MPI_DOUBLE,2, MPI_COMM_WORLD);
-	if (ProcRank == 0)
+	MPI_Bcast(test, 5, MPI_INT,3, MPI_COMM_WORLD);
 	endwtime1 = MPI_Wtime();
 
 	for (int i = 0; i < psize; i++) {
 		if (ProcRank == i) {
-			printf("Process %i : %f\n", ProcRank, test[2]);
+			//printf("Process %i : %d\n", ProcRank, test[4]);
 		}
 	}
 	
